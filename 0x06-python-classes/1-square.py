651bb775b7ccf0a4fdc4dac3adb17e6354a9e661
Raymond Toritseju Nesiama <raynesiama@gmail.com>
2023-03-28 14:29:08 +0400

    task update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+# 0-square.py by Raymond Nesiama
+
+"""Defines a square """
+
+
+class Square:
+    """Represents a square"""
+
+    def __init__(self, size):
+        """Initializing this square class
+        Args: size - represnets the size of the square defined
+        """
+
+        self.__size = size
